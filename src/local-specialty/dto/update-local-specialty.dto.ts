220aaa88d5af7a36e01ceee29490f0d3a683288d
NSHkw <64311828+NSHkw@users.noreply.github.com>
2024-11-25 10:00:22 +0900

    add: specaulty mvp

--- src/local-specialty/dto/update-local-specialty.dto.ts
+++ src/local-specialty/dto/update-local-specialty.dto.ts
@@ -1,4 +1,23 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLocalSpecialtyDto } from './create-local-specialty.dto';
+import { IsOptional, IsString, IsNotEmpty, MaxLength, IsEnum } from 'class-validator';
+import { Region } from '../types/region.type';
 
-export class UpdateLocalSpecialtyDto extends PartialType(CreateLocalSpecialtyDto) {}
+export class UpdateLocalSpecialtyDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  image_url?: string;
+
+  @IsOptional()
+  @IsEnum(Region)
+  @MaxLength(100)
+  region?: Region;
+
+  @IsOptional()
+  @IsString()
+  season_info?: string;
+}
