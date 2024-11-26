154cd25a103cdd5899669ca504f1b3edb3235d01
NSHkw <64311828+NSHkw@users.noreply.github.com>
2024-11-26 20:57:56 +0900

    fix: module update

--- src/cart-item/cart-item.module.ts
+++ src/cart-item/cart-item.module.ts
@@ -1,13 +1,19 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { forwardRef, Module } from '@nestjs/common';
 import { CartItemService } from './cart-item.service';
 import { CartItemController } from './cart-item.controller';
-import { AuthModule } from 'src/auth/auth.module';
-import { StoreProductModule } from 'src/store-product/store-product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartItem } from './entities/cart-item.entity';
+import { UserModule } from 'src/user/user.module';
+import { StoreProductModule } from 'src/store-product/store-product.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CartItem]), AuthModule, forwardRef(() => StoreProductModule)],
+  imports: [
+    TypeOrmModule.forFeature([CartItem]),
+    AuthModule,
+    forwardRef(() => UserModule),
+    forwardRef(() => StoreProductModule),
+  ],
   controllers: [CartItemController],
   providers: [CartItemService],
 })
