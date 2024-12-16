0debc03b28324357d9e3e43cc2bf025ca2ba89fa
NSHkw <64311828+NSHkw@users.noreply.github.com>
2024-12-16 21:09:08 +0900

    add: client update

--- src/local-specialty/local-specialty.service.ts
+++ src/local-specialty/local-specialty.service.ts
@@ -1,7 +1,7 @@
 // src/local-specialty/local-specialty.service.ts
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { LocalSpecialty } from './entities/local-specialty.entity';
-import { IsNull, Like, Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { Region } from './types/region.type';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SearchLocalSpecialtyDto } from './dto/search-local-specialty.dto';
@@ -13,6 +13,7 @@ const SPECIALTY_SELECT_FIELDS = {
     name: true,
     season_info: true,
     region: true,
+    image: true,
   },
   DETAIL: {
     id: true,
@@ -20,7 +21,7 @@ const SPECIALTY_SELECT_FIELDS = {
     description: true,
     season_info: true,
     region: true,
-    created_at: true,
+    image: true,
   },
 } as const;
 
